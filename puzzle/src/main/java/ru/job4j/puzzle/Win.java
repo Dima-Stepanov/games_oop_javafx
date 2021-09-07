0ff7a3d8896a961c7bb1aff0c79c32ec1a1b7cff
dmitryjob4j <haoos@inbox.ru>
2021-09-08 00:57:19 +0300

    Базовый синтаксис.2.Сокобан.Тестовое задание.[#75283#127171]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,8 +1,44 @@
 package ru.job4j.puzzle;
 
+/**
+ * Базовый синтаксис
+ * 1.1.7.Контрольные вопросы и тестовое задание.
+ * 2.Сокобан.Тестовое задание.[#75283#127171]
+ * Класс Win осуществляет проверку выигрышной комбинации на игровом поле
+ *
+ * @version dmitryjob4j
+ * @since 08.09.2021
+ */
 public class Win {
     public static boolean check(int[][] board) {
+        boolean rsl = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1 && (checkHor(board, i) || checkVer(board, i))) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean checkHor(int[][] board, int row) {
         boolean rsl = true;
+        for (int cell : board[row])
+            if (cell == 0) {
+                rsl = false;
+                break;
+            }
+        return rsl;
+    }
+
+    public static boolean checkVer(int[][] board, int cell) {
+        boolean rsl = true;
+        for (int[] row : board) {
+            if (row[cell] == 0) {
+                rsl = false;
+                break;
+            }
+        }
         return rsl;
     }
 }
--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -5,8 +5,14 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
-
-@Ignore
+/**
+ * Базовый синтаксис
+ * 1.1.7.Контрольные вопросы и тестовое задание.
+ * 2.Сокобан.Тестовое задание.[#75283#127171]
+ * Класс Win осуществляет проверку выигрышной комбинации на игровом поле
+ * @version dmitryjob4j
+ * @since 08.09.2021
+ */
 public class WinTest {
     @Test
     public void whenVerticalWin() {
@@ -45,6 +51,18 @@ public class WinTest {
     }
 
     @Test
+    public void whenNotWinV() {
+        int[][] board = {
+                {0, 0, 1, 0, 0},
+                {0, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+        };
+        assertThat(Win.check(board), is(false));
+    }
+
+    @Test
     public void whenNotWinL() {
         int[][] board = {
                 {0, 0, 0, 0, 0},
@@ -54,5 +72,17 @@ public class WinTest {
                 {1, 1, 1, 1, 0},
         };
         assertThat(Win.check(board), is(false));
+    }
+
+    @Test
+    public void whenNotWinMy() {
+        int[][] board = {
+                {0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1},
+                {0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1},
+                {1, 1, 1, 1, 0},
+        };
+        assertFalse(Win.check(board));
     }
 }
